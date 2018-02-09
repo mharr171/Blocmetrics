4b02e9d236b5c912936f72f852e4328801d71c6a
Matthew Harris <mharr171.z@gmail.com>
2018-02-08 17:19:05 -0800

    added Applications, implemented CRUD for Applications

---
+++ app/models/application.rb
@@ -0,0 +1,4 @@
+class Application < ApplicationRecord
+  validates :url, uniqueness: { case_sensitive: false, message:"This url already exists in an existing application." }
+  belongs_to :user
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create My Account
+my_account = User.new(
+  first_name: 'Matthew',
+  last_name:  'Harris',
+  email:      'mharr171.z@gmail.com',
+  password:   'asdf123'
+)
+my_account.skip_confirmation!
+my_account.save!
+print 'My account (mharr171.z@gmail.com) created.'
+
+# Create My Account
+my_account = User.new(
+  first_name: 'Matt',
+  last_name:  'Harris',
+  email:      'matt@livefullstack.com',
+  password:   'asdf123'
+)
+my_account.skip_confirmation!
+my_account.save!
+print 'My account (matt@livefullstack.com) created.'
