a62ebc9f484cd8dfbaf369efe4f72d2ba8e5b9bd
Matthew Harris <mharr171.z@gmail.com>
2018-02-13 04:20:36 -0800

    make events dependent

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
   validates :url, uniqueness: { case_sensitive: false, message:"This url already exists in an existing application." }
   belongs_to :user
-  has_many :events
+  has_many :events, dependent: :destroy
 end
